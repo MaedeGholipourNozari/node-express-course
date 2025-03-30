c0b732072b9c1d80999b6c3e54932d5a11d457a3
Maede Gholipour <maedegholipour@gmail.com>
2025-03-30 16:30:38 -0400

    NPM and Async Patterns

---
+++ 01-node-tutorial/answers/writeWithPromisesAwait.js
@@ -0,0 +1,31 @@
+const { writeFile, readFile } = require("fs").promises;  
+
+
+async function writer() {
+    try {
+        await writeFile("temp.txt", "First line\n");
+        await writeFile("temp.txt", "Second line\n", { flag: "a" }); // Append mode
+        await writeFile("temp.txt", "Third line\n", { flag: "a" }); // Append mode
+        
+        console.log("File written successfully");
+    } catch (error) {
+        console.error("Error writing file:", error);
+    }
+}
+
+
+async function reader() {
+    try {
+        const data = await readFile("temp.txt", "utf8");
+        console.log("File contents:\n", data);
+    } catch (error) {
+        console.error("Error reading file:", error);
+    }
+}
+
+async function readWrite() {
+    await writer();
+    await reader();
+}
+
+readWrite();
